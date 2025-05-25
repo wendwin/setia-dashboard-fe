15bf35b2fcfbe1b63a3e1685b6c58222290ef39f
wendwin <wendwin12@gmail.com>
2025-05-26 05:58:27 +0700

    memperbaiki navabr

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -86,8 +86,7 @@
                   </template>
                 </button>
               </li>
-              <!-- Notifications menu -->
-              <li class="relative">
+              <!-- <li class="relative">
                 <button
                   class="relative align-middle rounded-md focus:outline-none focus:shadow-outline-purple"
                   @click="toggleNotificationsMenu"
@@ -105,7 +104,6 @@
                       d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"
                     ></path>
                   </svg>
-                  <!-- Notification badge -->
                   <span
                     aria-hidden="true"
                     class="absolute top-0 right-0 inline-block w-3 h-3 transform translate-x-1 -translate-y-1 bg-red-600 border-2 border-white rounded-full dark:border-gray-800"
@@ -156,9 +154,9 @@
                     </li>
                   </ul>
                 </template>
-              </li>
+              </li> -->
               <!-- Profile menu -->
-              <li class="relative">
+              <!-- <li class="relative">
                 <button
                   class="align-middle rounded-full focus:shadow-outline-purple focus:outline-none"
                   @click="toggleProfileMenu"
@@ -252,7 +250,7 @@
                     </li>
                   </ul>
                 </template>
-              </li>
+              </li> -->
             </ul>
           </div>
         </header>
