ad0fc0c7122c2f7c337f90dec2df5cae216f928b
wendwin <wendwin12@gmail.com>
2025-05-26 05:56:02 +0700

    menambahkan menu topic analysis

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import DashboardPage from '../views/DashboardPage.vue';
 import DataReviews from '../views/DataReviews.vue';
 import DataPreprocessing from '../views/DataPrerocessing.vue';
 import SentimentAnalysis from "../views/SentimentAnalysis.vue";
+import TopicAnalysis from "../views/TopicAnalysis.vue";
 
 const router = createRouter({
     routes: [
@@ -15,6 +16,7 @@ const router = createRouter({
               { path: '/data-reviews', name: 'data-reviews', component: DataReviews },
               { path: '/pre-processing/:step', name: 'preprocessing', component: DataPreprocessing },
               { path: '/sentiment-analysis', name: 'sentiment-analysis', component: SentimentAnalysis },
+              { path: '/topic-analysis', name: 'topic-analysis', component: TopicAnalysis },
             ]
         },
     ],
