ea36c5dfbf52da5ea808a7b8dde7f5cfa8d3ae10
wendwin <wendwin12@gmail.com>
2025-07-12 06:12:20 +0700

    merubah tampilan login dan edit modal

--- src/components/ModalEditSummary.vue
+++ src/components/ModalEditSummary.vue
@@ -78,7 +78,8 @@ async function saveChanges() {
         const res = await fetch(`${baseUrl}/api/summaries/bulk-update`, {
             method: 'PUT',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('access_token')}`
             },
             body: JSON.stringify(payload)
         })
