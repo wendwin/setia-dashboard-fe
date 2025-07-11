615edee6b4ad24ba8eb67a6ea67df3cf95783040
wendwin <wendwin12@gmail.com>
2025-07-12 06:48:20 +0700

    perubahan topic highlights

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,7 +3,7 @@
     <!-- Desktop sidebar -->
     <aside class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-shrink-0">
       <div class="py-4 text-gray-500 dark:text-gray-400">
-        <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
+        <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="/">
           STIAMU
         </a>
         <ul class="mt-6">
@@ -168,7 +168,7 @@
         v-if="isSideMenuOpen" id="sidebar">
 
         <div class="py-4 text-gray-500 dark:text-gray-400">
-          <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
+          <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="/">
             STIAMU
           </a>
           <ul class="mt-6">
